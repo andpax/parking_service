bc94fe1bdd64a590e15489a3cb027b4b03688072
Anderson Nascimento <and.pax@gmail.com>
2025-05-21 20:32:40 -0300

    Criação de filtros para Customers, Parking e Vehicles

--- core/settings.py
+++ core/settings.py
@@ -284,6 +284,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_FILTER_BACKENDS': ['dj_rql.drf.RQLFilterBackend'],
     #  'DEFAULT_PERMISSION_CLASSES': [
     #     'rest_framework.permissions.IsAuthenticated',
     # ],
